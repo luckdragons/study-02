9f3f02466ca0c3d09f968cbc27b578c1da1a233b
ext-lihb <jerry_lee0801@163.com>
2020-03-31 09:50:54 +0800

    首次提交项目

---
+++ src/main/java/com/mystudy/IndexContorller.java
@@ -0,0 +1,13 @@
+package com.mystudy;
+
+/**
+ * @ProjectName: study-02
+ * @Package: com.mystudy
+ * @ClassName: IndexContorller
+ * @Author: ext-lihb
+ * @Description: ${description}
+ * @Date: 2020/3/31 9:32
+ * @Version: 1.0
+ */
+public class IndexContorller {
+}
